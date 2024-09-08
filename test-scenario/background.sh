cb6d151b615ed42005f0d2c6235f9740fc20588e
panosmaurikos <72733593+panosmaurikos@users.noreply.github.com>
2024-09-08 18:11:27 +0300

    Update background.sh

--- test-scenario/background.sh
+++ test-scenario/background.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 sudo apt-get remove -y golang-go
+
 sudo apt-get autoremove -y
 
 
 GO_VERSION="1.23.1"
+
 wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
 
 sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 
 
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+
 source ~/.bashrc
 
 
