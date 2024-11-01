70aefb8f04efd943fce9a9b84dcdbe0e6c45218f
panosmaurikos <72733593+panosmaurikos@users.noreply.github.com>
2024-11-01 22:07:28 +0200

    Update background.sh

--- CRD_Etherpad/background.sh
+++ CRD_Etherpad/background.sh
@@ -15,3 +15,56 @@ sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
 
 source ~/.bashrc
+
+sudo mkdir -p /mnt/data/etherpad /mnt/data/mysql
+
+
+cat <<EOF > pv-pvc.yml
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: etherpad-pv
+spec:
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: "/mnt/data/etherpad"
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: etherpad-pvc
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: mysql-pv
+spec:
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: "/mnt/data/mysql"
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: mysql-pvc
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+EOF
+
+kubectl apply -f pv-pvc.yml
